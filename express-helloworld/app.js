08b525c15eb8a0911a615b02273f5ba7c3541ec8
skandanagowda <skandanagowda20@gmail.com>
2024-05-09 23:57:57 -0400

    updated app to v3

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,9 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  response = 'This is version 2 of the app.' + '\n';
+  response = 'This is version 3 of the app.' + '\n';
+
   //send the response to the client
   res.send(response);
+
 });
 
 app.listen(8080, function () {
